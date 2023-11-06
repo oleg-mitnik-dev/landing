94a4c0889c5860c6e238810a874e2a80ea5e8d10
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-11-06 15:13:44 +0300

    lg:layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html',],
+  content: ['./index.html','./main.html'],
   theme: {
     extend: {
       backgroundImage: {
@@ -9,9 +10,30 @@ module.exports = {
         'cows-img': "url('/assets/images/cows.jpg')",
         'farmland-img': "url('/assets/images/farmland.jpg')",
 
-      }
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+
+    // Adding text-shadow classes
+    // so we can write something like this `<div class="text-shadow-sm shadow-red-500" ...>`
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities({
+        'text-shadow': (value) => ({
+          textShadow: value,
+        }),
+      },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
+
+
 
